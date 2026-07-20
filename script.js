5dc0708994754884a94a064f794bd2180c2315e7
Diogo Matias <diogo.torres.matias@tecnico.ulisboa.pt>
2026-07-21 00:27:46 +0100

    language selection

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// map
+
 var imageWidth = 3164;
 var imageHeight = 2344;
 
@@ -9,6 +11,54 @@ var map = L.map('map', {
 var bounds = [[0, 0], [imageHeight, imageWidth]];
 L.imageOverlay('images/westeros.jpg', bounds).addTo(map);
 map.fitBounds(bounds);
+
+// language & search
+
+const input = document.getElementById("searchInput");
+const langSelect = document.getElementById("language");
+let lang = langSelect.value;
+let fuse;
+
+langSelect.addEventListener("change", async function () {
+    lang = langSelect.value;
+    const translations = await loadTranslations(lang);
+    buildFuseIndex(translations);
+});
+
+async function loadTranslations(lang) {
+    const response = await fetch(`translations/${lang}.json`);
+    const data = await response.json();
+    return data;
+}
+
+function buildSearchArray(translations) {
+    return Object.entries(translations).map(([id, name]) => ({ id, name }));
+}
+
+function buildFuseIndex(translations) {
+    const searchArray = buildSearchArray(translations);
+    fuse = new Fuse(searchArray, {
+        keys: ["name"],
+        threshold: 0.3
+    });
+}
+
+input.addEventListener("input", function () {
+    const text = input.value;
+
+    if (!text) return;
+    
+    const results = fuse.search(text);
+    console.log(results);
+});
+
+function init() {
+    loadTranslations(lang).then(translations => {
+        buildFuseIndex(translations);
+    });
+}
+
+init();
 
 L.marker([1180, 2581]).addTo(map);
 L.marker([1344, 759]).addTo(map);
