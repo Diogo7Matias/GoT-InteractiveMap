33cd5e992e1faaf2568dc88b9a615865fe5ee375
Diogo Matias <diogo.torres.matias@tecnico.ulisboa.pt>
2026-07-23 20:41:13 +0100

    feature: search zoom in

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// map
+// map setup
 
 var imageWidth = 3164;
 var imageHeight = 2344;
@@ -12,12 +12,33 @@ var bounds = [[0, 0], [imageHeight, imageWidth]];
 L.imageOverlay('images/westeros.jpg', bounds).addTo(map);
 map.fitBounds(bounds);
 
+// load JSON data
+
+async function loadJSON(filename) {
+    const response = await fetch(filename);
+    const data = await response.json();
+    return data;
+}
+
+async function loadTranslations(lang) {
+    return await loadJSON(`translations/${lang}.json`);
+}
+
+async function loadLocations() {
+    return await loadJSON('locations.json');
+}
+
 // language & search
 
 const input = document.getElementById("searchInput");
 const langSelect = document.getElementById("language");
 let lang = langSelect.value;
 let fuse;
+let locations;
+
+init();
+
+input.addEventListener("input", () => search(input.value));
 
 langSelect.addEventListener("change", async function () {
     lang = langSelect.value;
@@ -25,14 +46,12 @@ langSelect.addEventListener("change", async function () {
     buildFuseIndex(translations);
 });
 
-async function loadTranslations(lang) {
-    const response = await fetch(`translations/${lang}.json`);
-    const data = await response.json();
-    return data;
-}
-
 function buildSearchArray(translations) {
     return Object.entries(translations).map(([id, name]) => ({ id, name }));
+}
+
+function buildLocationsMap(locationsArray) {
+    return new Map(locationsArray.map(loc => [loc.id, loc]));
 }
 
 function buildFuseIndex(translations) {
@@ -43,22 +62,25 @@ function buildFuseIndex(translations) {
     });
 }
 
-input.addEventListener("input", function () {
-    const text = input.value;
-
-    if (!text) return;
+function search(text) {
+    if (!text) {
+        map.flyTo([imageHeight / 2, imageWidth / 2], -2, {duration: 0.4});
+        return;
+    }
     
     const results = fuse.search(text);
-    console.log(results);
-});
+    const locID = results[0].item.id;
+    const location = locations.get(locID);
+
+    map.flyTo([location.y, location.x], 0.5, {duration: 0.8});
+}
 
 function init() {
     loadTranslations(lang).then(translations => {
         buildFuseIndex(translations);
     });
+
+    loadLocations().then(locs => {
+        locations = buildLocationsMap(locs);
+    });
 }
-
-init();
-
-L.marker([1180, 2581]).addTo(map);
-L.marker([1344, 759]).addTo(map);
